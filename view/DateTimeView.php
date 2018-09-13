41e6f28e8eb73603b1d22a9ed7dfedd1faf6c821
sb22ix <sb22ix@student.lnu.se>
2018-09-13 19:53:18 +0200

    Latest commit

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,14 +5,9 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeStringDay = date('l');
-		$timeStringDate = date('jS');
-		$timeStringMonth = date('F');
-		$timeStringYear = date('Y');
-		$timeStringTime = date('h:i:s');
+		$timeString = date('l jS \of F Y  h:i:s');
+		
 
-		//  jS \of F Y h:i:s
-
-		return '<p>'" . $timeStringDay ., the . $timeStringDate . of . $timeStringMonth . . $timeStringYear ., The time is . $timeStringTime . " '</p>';
+		return '<p>' . $timeString . '</p>';
 	}
 }
