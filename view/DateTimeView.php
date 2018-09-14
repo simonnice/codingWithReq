40317aa68676f7b0aba4a167cfa6bf187196f400
sb22ix <sb22ix@student.lnu.se>
2018-09-14 11:09:10 +0200

    Working on stringing together correct DateTime

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,9 +5,10 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = date('l jS \of F Y  h:i:s');
-		echo "Hej";
-		
+		$timeString = // $timeString = date('l jS \of F Y \,  h:i:s');
+		$timeString = date('l');
+		$timeString .=", the " . date('jS');
+				
 
 		return '<p>' . $timeString .  '</p>';
 	}
