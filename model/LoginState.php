5c5e148d462cbdea180aeb23c64b16eff0cbb1dd
sb22ix <sb22ix@student.lnu.se>
2018-09-14 14:19:06 +0200

    Adding first error message to checkInputData function in LoginState

--- model/LoginState.php
+++ model/LoginState.php
@@ -5,6 +5,8 @@ namespace model;
 class LoginState {
 
     public function checkInputData($loginData) {
-
+        if ($loginData['LoginView::UserName'] == null) {
+            return "Username is missing";
+        }
     }
 }
