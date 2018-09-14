62bd643a7909fa634b046976593aebfb5ec2eaa7
sb22ix <sb22ix@student.lnu.se>
2018-09-14 11:15:45 +0200

    Commiting hopefully correct string of datetime to fill requirement

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -8,8 +8,14 @@ class DateTimeView {
 		$timeString = // $timeString = date('l jS \of F Y \,  h:i:s');
 		$timeString = date('l');
 		$timeString .=", the " . date('jS');
+		$timeString .=" of " . date('F');
+		$timeString .=" " . date('Y');
+		$timeString .=", The time is " . date('h:i:s');
 				
 
 		return '<p>' . $timeString .  '</p>';
 	}
 }
+
+
+// Friday, the 14th of September 2018, The time is '
