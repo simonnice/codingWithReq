507f72d5e9dae631a101959fe0d863be9a514eab
sb22ix <sb22ix@student.lnu.se>
2018-09-17 14:59:49 +0200

    Adding some static private variables for holding form info

--- view/RegisterView.php
+++ view/RegisterView.php
@@ -4,4 +4,7 @@ namespace view;
 
 class RegisterView {
 
+    private static $register = 'RegisterView::Register';
+    private static $name = 'RegisterView::UserName';
+    private static $password = 'RegisterView::Password';
 }
