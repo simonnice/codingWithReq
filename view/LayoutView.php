9c3c74f022006ab3dde638620339c6ea0e0a8829
sb22ix <sb22ix@student.lnu.se>
2018-09-17 15:49:24 +0200

    Working with getting the correct html view for logged in users and not logged in

--- view/LayoutView.php
+++ view/LayoutView.php
@@ -5,27 +5,48 @@ class LayoutView {
 
     public function render($isLoggedIn, LoginView $v, DateTimeView $dtv, $lc) {
 
-        $responseValue = $lc->checkLoginCredentials();
+        if ($isLoggedIn == false) {
+            $responseValue = $lc->checkLoginCredentials();
+            echo '<!DOCTYPE html>
+          <html>
+            <head>
+              <meta charset="utf-8">
+              <title>Login Example</title>
+            </head>
+            <body>
+              <h1>Assignment 2</h1>
+              <a href="register.php"> Register a new user</a>
+              ' . $this->renderIsLoggedIn($isLoggedIn) . '
 
-        echo '<!DOCTYPE html>
-      <html>
-        <head>
-          <meta charset="utf-8">
-          <title>Login Example</title>
-        </head>
-        <body>
-          <h1>Assignment 2</h1>
-          <a href="register.php"> Register a new user</a>
-          ' . $this->renderIsLoggedIn($isLoggedIn) . '
+              <div class="container">
+                  ' . $v->response($responseValue) . '
 
-          <div class="container">
-              ' . $v->response($responseValue) . '
+                  ' . $dtv->show() . '
+              </div>
+             </body>
+          </html>
+        ';
+        } else if ($isLoggedIn == true) {
+            echo '<!DOCTYPE html>
+            <html>
+              <head>
+                <meta charset="utf-8">
+                <title>Login Example</title>
+              </head>
+              <body>
+                <h1>Assignment 2</h1>
+                ' . $this->renderIsLoggedIn($isLoggedIn) . '
+                    <p>Welcome</p>
+                <div class="container">
+                <input type="submit" id="submit" name="' . self::$logout . '" value="logout" />
 
-              ' . $dtv->show() . '
-          </div>
-         </body>
-      </html>
-    ';
+                    ' . $dtv->show() . '
+                </div>
+               </body>
+            </html>
+            ';
+        }
+
     }
 
     private function renderIsLoggedIn($isLoggedIn) {
