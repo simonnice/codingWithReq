1cf9368b37e4bc74755e147ba799c6721e32bab7
sb22ix <sb22ix@student.lnu.se>
2018-09-17 15:02:41 +0200

    Beginning to build html for RegisterView

--- view/RegisterView.php
+++ view/RegisterView.php
@@ -7,4 +7,17 @@ class RegisterView {
     private static $register = 'RegisterView::Register';
     private static $name = 'RegisterView::UserName';
     private static $password = 'RegisterView::Password';
+
+    /**
+     * Generate HTML code on the output buffer for the register button
+     * @param $message, String output message
+     * @return  void, BUT writes to standard output!
+     */
+
+     private function generateRegisterFormHTML($message) {
+         return '
+            <form method="post">
+                <fieldset>
+                    <legend>Register a new user - Write username and password</legend>
+     }
 }
