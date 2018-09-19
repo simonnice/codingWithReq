dd43c0a2ef5c2e0cb62d6930175c48ccd7e3a12f
sb22ix <sb22ix@student.lnu.se>
2018-09-19 16:56:25 +0200

    Disabling error handling for public server

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ require_once 'model/LoginState.php';
 require_once 'model/RegisterState.php';
 
 //MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
-error_reporting(E_ALL);
-ini_set('display_errors', 'On');
+//error_reporting(E_ALL);
+//ini_set('display_errors', 'On');
 
 //CREATE OBJECTS OF THE VIEWS
 $loginView = new \view\LoginView();
