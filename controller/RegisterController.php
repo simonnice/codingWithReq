40fc0325780da3ef1a0d7213a600ee688997d269
sb22ix <sb22ix@student.lnu.se>
2018-09-19 17:48:26 +0200

    Changes!

--- controller/RegisterController.php
+++ controller/RegisterController.php
@@ -1,28 +1,20 @@
 <?php
-
 namespace controller;
 
 class registerController {
-
     private $state;
     private $registerView;
-
     public function __construct($state, $register) {
         $this->state = $state;
         $this->registerView = $register;
     }
-
     /**
      * Handle a GET request to Register.php
      *testing
      */
-
     public function checkIfRegisterIsClicked() {
         if ($this->registerView->registerLinkIsClicked() == true) {
-
             return true;
         }
-
     }
-
 }
