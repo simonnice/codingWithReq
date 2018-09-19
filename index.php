207a7523c9844cc05c6dc91b87d4253539dd8066
sb22ix <sb22ix@student.lnu.se>
2018-09-19 17:09:12 +0200

    Potential fix to serverIssue, registerView typo

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 //INCLUDE THE FILES NEEDED...
 require_once 'view/LoginView.php';
-require_once 'view/registerView.php';
+require_once 'view/RegisterView.php';
 require_once 'view/DateTimeView.php';
 require_once 'view/LayoutView.php';
 require_once 'controller/LoginController.php';
