ee74396dc68dfc36a52279439389f7c0da938fbc
sb22ix <sb22ix@student.lnu.se>
2018-09-22 11:10:32 +0200

    Sanitizing inputs before checking them in RegisterState

--- model/RegisterState.php
+++ model/RegisterState.php
@@ -4,7 +4,10 @@ namespace model;
 
 class RegisterState {
 
-    public function ValidateRegisterInputData() {
+    public function ValidateRegisterInputData($data) {
+
+        $sanitizedName = filter_var($data['RegisterView::UserName'], FILTER_SANITIZE_STRING);
+        $sanitizedPassword = filter_var($data['RegisterView::Password'], FILTER_SANITIZE_STRING);
 
     }
 }
