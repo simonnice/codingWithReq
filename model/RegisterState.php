476daab2fdd7199c5c70041c1b32bf7866b3fbe1
sb22ix <sb22ix@student.lnu.se>
2018-09-22 11:15:52 +0200

    Finishing validation in RegisterState method

--- model/RegisterState.php
+++ model/RegisterState.php
@@ -8,6 +8,15 @@ class RegisterState {
 
         $sanitizedName = filter_var($data['RegisterView::UserName'], FILTER_SANITIZE_STRING);
         $sanitizedPassword = filter_var($data['RegisterView::Password'], FILTER_SANITIZE_STRING);
+        $sanitizedRepeatPassword = filter_var($data['RegisterView::PasswordRepeat'], FILTER_SANITIZE_STRING);
+
+        if (strlen($sanitizedName) < 3) {
+            throw new \Exception('Username has too few characters, at least 3 characters.');
+        } else if (strlen($sanitizedPassword) < 6) {
+            throw new \Exception('Password has too few characters, at least 6 characters.');
+        } else if ($sanitizedPassword == $sanitizedRepeatPassword) {
+            throw new \Exception('Passwords do not match.');
+        }
 
     }
 }
