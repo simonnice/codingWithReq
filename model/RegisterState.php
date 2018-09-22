989f87c2fb00e5feaf5bf20e3a3acfac44719fc9
sb22ix <sb22ix@student.lnu.se>
2018-09-22 17:00:45 +0200

    Adding some validation

--- model/RegisterState.php
+++ model/RegisterState.php
@@ -14,7 +14,11 @@ class RegisterState {
         $sanitizedPassword = filter_var($data['RegisterView::Password'], FILTER_SANITIZE_SPECIAL_CHARS);
         $sanitizedRepeatPassword = filter_var($data['RegisterView::PasswordRepeat'], FILTER_SANITIZE_SPECIAL_CHARS);
 
-        if (strlen($sanitizedName) < 3) {
+        if (empty($sanitizedName) == true && empty($sanitizedPassword) == true) {
+            throw new \Exception("Username has too few characters, at least 3 characters. <br>
+            Password has too few characters, at least 6 characters. <br>
+            ");
+        } else if (strlen($sanitizedName) < 3) {
             throw new \Exception("Username has too few characters, at least 3 characters.");
         } else if (strlen($sanitizedPassword) < 6) {
             throw new \Exception("Password has too few characters, at least 6 characters.");
