51a30798d9e6b3797d881e3c30cccafee80690af
sb22ix <sb22ix@student.lnu.se>
2018-09-22 14:01:23 +0200

    Fixing

--- view/RegisterView.php
+++ view/RegisterView.php
@@ -24,7 +24,7 @@ class RegisterView {
     }
 
     public function generateRegisterLink() {
-        $registerLink = 'register?';
+        $registerLink = '?register';
         return '
         <a href="' . $registerLink . '">Register a new user</a>
 		';
@@ -39,7 +39,7 @@ class RegisterView {
     private function generateRegisterFormHTML($message) {
         return '
         <h2>Register new user</h2>
-            <form action ="register?" form method="post">
+            <form action ="?register" form method="post">
                 <fieldset>
                     <legend>Register a new user - Write username and password</legend>
                     <p id="' . self::$messageId . '">' . $message . '</p>
