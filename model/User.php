1d66166f0c08a65c2baadcec5447b9bc8ba13d58
sb22ix <sb22ix@student.lnu.se>
2018-09-24 11:43:35 +0200

    Adding constructor to User class

--- model/User.php
+++ model/User.php
@@ -4,6 +4,12 @@ namespace model;
 
 class User{
     private $userName;
-    private $passWord
+    private $password
     private $loggedIn;
+
+    public function __construct($userName, $password, $loggedIn) {
+        $this->userName = $userName;
+        $this->password = $password;
+        $this->loggedIn = $loggedIn;
+    }
 }
