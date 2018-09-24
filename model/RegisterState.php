72a51b377a8aa1b1bc2a1ddcd56d52fd56b0d884
sb22ix <sb22ix@student.lnu.se>
2018-09-24 13:48:55 +0200

    Testing register error input

--- model/RegisterState.php
+++ model/RegisterState.php
@@ -6,22 +6,23 @@ class RegisterState {
 
     public function ValidateRegisterInputData($data) {
 
-        $sanitizedName = filter_var($data['RegisterView::UserName'], FILTER_SANITIZE_STRING);
+        /*$sanitizedName = filter_var($data['RegisterView::UserName'], FILTER_SANITIZE_STRING);
         $sanitizedPassword = filter_var($data['RegisterView::Password'], FILTER_SANITIZE_STRING);
         $sanitizedRepeatPassword = filter_var($data['RegisterView::PasswordRepeat'], FILTER_SANITIZE_STRING);
-
+         *//*
         $sanitizedName = filter_var($data['RegisterView::UserName'], FILTER_SANITIZE_SPECIAL_CHARS);
         $sanitizedPassword = filter_var($data['RegisterView::Password'], FILTER_SANITIZE_SPECIAL_CHARS);
         $sanitizedRepeatPassword = filter_var($data['RegisterView::PasswordRepeat'], FILTER_SANITIZE_SPECIAL_CHARS);
+         */
 
-        if (empty($sanitizedName) == true && empty($sanitizedPassword) == true) {
+        if (empty($data['RegisterView::UserName']) == true && empty($data['RegisterView::Password']) == true) {
             throw new \Exception("Username has too few characters, at least 3 characters.<br>
             Password has too few characters, at least 6 characters.<br> ");
-        } else if (strlen($sanitizedName) < 3) {
+        } else if (strlen($data['RegisterView::UserName']) < 3) {
             throw new \Exception("Username has too few characters, at least 3 characters.");
-        } else if (strlen($sanitizedPassword) < 6) {
+        } else if (strlen($data['RegisterView::Password']) < 6) {
             throw new \Exception("Password has too few characters, at least 6 characters.");
-        } else if ($sanitizedPassword != $sanitizedRepeatPassword) {
+        } else if ($data['RegisterView::Password'] != $data['RegisterView::PasswordRepeat']) {
             throw new \Exception("Passwords do not match.");
         }
 
