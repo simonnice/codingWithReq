29bd9ce8fa944d8ea6f1c7978f1a09fa368a0210
sb22ix <sb22ix@student.lnu.se>
2018-09-24 09:11:00 +0200

    Testing DB on live server

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-define('ROOT_URL', 'http://localhost/codingWithReq/');
+define('ROOT_URL', 'http://104.248.170.11/codingWithReq/');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', 'wiolethstaffan1');
