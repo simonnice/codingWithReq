0a4983854a42005356b0ea30d52cbb33c33a7f59
sb22ix <sb22ix@student.lnu.se>
2018-09-24 14:00:02 +0200

    Testing to test strings in separate if statements

--- model/RegisterState.php
+++ model/RegisterState.php
@@ -6,24 +6,29 @@ class RegisterState {
 
     public function ValidateRegisterInputData($data) {
 
-        /*$sanitizedName = filter_var($data['RegisterView::UserName'], FILTER_SANITIZE_STRING);
+        $sanitizedName = filter_var($data['RegisterView::UserName'], FILTER_SANITIZE_STRING);
         $sanitizedPassword = filter_var($data['RegisterView::Password'], FILTER_SANITIZE_STRING);
         $sanitizedRepeatPassword = filter_var($data['RegisterView::PasswordRepeat'], FILTER_SANITIZE_STRING);
-         *//*
+
         $sanitizedName = filter_var($data['RegisterView::UserName'], FILTER_SANITIZE_SPECIAL_CHARS);
         $sanitizedPassword = filter_var($data['RegisterView::Password'], FILTER_SANITIZE_SPECIAL_CHARS);
         $sanitizedRepeatPassword = filter_var($data['RegisterView::PasswordRepeat'], FILTER_SANITIZE_SPECIAL_CHARS);
-         */
 
-        if (empty($data['RegisterView::UserName']) == true && empty($data['RegisterView::Password']) == true) {
-            throw new \Exception("Username has too few characters, at least 3 characters.<br>
-            Password has too few characters, at least 6 characters.<br> ");
-        } else if (strlen($data['RegisterView::UserName']) < 3) {
-            throw new \Exception("Username has too few characters, at least 3 characters.");
-        } else if (strlen($data['RegisterView::Password']) < 6) {
-            throw new \Exception("Password has too few characters, at least 6 characters.");
-        } else if ($data['RegisterView::Password'] != $data['RegisterView::PasswordRepeat']) {
+        if ($sanitizedPassword != $sanitizedRepeatPassword) {
             throw new \Exception("Passwords do not match.");
+        }
+
+        if (strlen($sanitizedPassword) < 6) {
+            throw new \Exception("Password has too few characters, at least 6 characters.");
+        }
+
+        if (strlen($sanitizedName) < 3) {
+            throw new \Exception("Username has too few characters, at least 3 characters.");
+        }
+
+        if (empty($sanitizedName) == true && empty($sanitizedPassword) == true) {
+            throw new \Exception("Username has too few characters, at least 3 characters.
+            Password has too few characters, at least 6 characters.");
         }
 
     }
