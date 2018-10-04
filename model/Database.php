5cbb3e4880fe3bec078829ff8d4f50ed33df0f70
sb22ix <sb22ix@student.lnu.se>
2018-10-04 12:50:10 +0200

    Setting some private variables in the new Database class to represent the credentials of the mysql database

--- model/Database.php
+++ model/Database.php
@@ -5,5 +5,9 @@
 // I'm starting with switching to prepared statements
 // Through the use of PDO and a Database class
 
-
-s
+class Database {
+    private $host = DB_HOST;
+    private $user = DB_USER;
+    private $pass = DB_PASS;
+    private $dbname = DB_NAME;
+}
