ffa1b010c6b39e9918ef4a56ef0d54aa1cf3b3fd
sb22ix <sb22ix@student.lnu.se>
2018-10-04 12:45:53 +0200

    Beginning refactor work on CodingWithReq. Working with new prepared statements through PDO in a separate Branch

---
+++ model/Database.php
@@ -0,0 +1,9 @@
+<?php
+
+// PDO Database class
+// This is part of the refactor of CodingWithReq
+// I'm starting with switching to prepared statements
+// Through the use of PDO and a Database class
+
+
+s
