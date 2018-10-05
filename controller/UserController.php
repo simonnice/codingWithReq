a191581f545fc6e00a334d7643aca423ac67688a
sb22ix <sb22ix@student.lnu.se>
2018-10-05 10:51:47 +0200

    Working to add similar structure for login as for register in UserController

--- controller/UserController.php
+++ controller/UserController.php
@@ -35,8 +35,8 @@ class UserController extends MainController {
 
     // returns the response from validation in User Model
 
-    public function validateFormData($data) {
-        return $validatedInput = $this->user->validateRegisterInputInForm($data);
+    public function validatetRegisterFormData($data) {
+        return $validatedRegisterInput = $this->user->validateRegisterInputInForm($data);
     }
 
     // Returns a boolean to determine if registration was successful or not
@@ -46,7 +46,7 @@ class UserController extends MainController {
         if ($this->registerView->isRegisterButtonClicked()) {
 
             $registerInput = $this->registerInputResponse();
-            $validatedData = $this->validateFormData($registerInput);
+            $validatedData = $this->validatetRegisterFormData($registerInput);
 
             if (empty($validatedData['name_err']) && empty($validatedData['password_err']) && empty($validatedData['confirm_password_err'])) {
                 $this->user->registerNewUser($validatedData);
@@ -85,6 +85,42 @@ class UserController extends MainController {
         ];
 
         return $data;
+    }
+
+    public function validatedLoginFormData($data) {
+        return $validatedLoginInput = $this->user->validateLoginInputInForm($data);
+    }
+
+    public function loginResponseFromDatabase() {
+
+        if ($this->registerView->isRegisterButtonClicked()) {
+
+            $registerInput = $this->registerInputResponse();
+            $validatedData = $this->validatetRegisterFormData($registerInput);
+
+            if (empty($validatedData['name_err']) && empty($validatedData['password_err']) && empty($validatedData['confirm_password_err'])) {
+                $this->user->registerNewUser($validatedData);
+                $this->user->handleUserResponse($validatedData);
+            } else {
+                $this->user->handleUserResponse($validatedData);
+            }
+
+        } else {
+
+            // Init data
+            $data = [
+                'name' => '',
+                'email' => '',
+                'password' => '',
+                'confirm_password' => '',
+                'name_err' => '',
+                'email_err' => '',
+                'password_err' => '',
+                'confirm_password_err' => '',
+            ];
+
+            return $data;
+        }
     }
 
 }
