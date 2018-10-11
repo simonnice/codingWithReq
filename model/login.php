597220ea95f44efd5edb46bf5c4fcdc6f0322c77
sb22ix <sb22ix@student.lnu.se>
2018-10-11 12:05:18 +0200

    Beginning work on Login class, handling all possible errors during creation of the object

--- model/login.php
+++ model/login.php
@@ -4,8 +4,30 @@ class Login {
 
     private $username;
     private $password;
-    
-    public __construct($userName, $password) {
+    private $db;
 
+    public __construct($userName, $password, $db) {
+        $this->db = $db;
+
+        if (!$userName) {
+            throw new \Exception("Username is missing");
+        }
+
+        if (!$password) {
+            throw new \Exception("Password is missing");
+        } 
+        
+        if (!$this->db->doesInputUserMatchDbUser($userName, $password)) {
+            throw new \Exception("Wrong name or password");
+        } 
+        
+        
+        else {
+            if ($this->session->isSessionSet()) {
+                $this->data['db_msg'] = "";
+            } else {
+                $this->data['db_msg'] = "Welcome";
+            }
+        }
     }
 }
