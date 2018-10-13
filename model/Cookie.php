bad61234ecb3092a46c51a37f9d90e3c36b6ef08
sb22ix <sb22ix@student.lnu.se>
2018-10-13 10:20:03 +0200

    Creating getter and setter for cookie name

--- model/Cookie.php
+++ model/Cookie.php
@@ -6,8 +6,25 @@ class Cookie {
 
     private $name;
     private $value = "";
+    private $time;
 
     public function __construct() {
 
     }
+
+    public function createCookie() {
+      return setCookie($this->name, $this->getValue())
+    }
+
+    public function setCookieName($name) {
+      $this->name = $name;
+    }
+
+    public function getCookieName() {
+      return $this->name;
+    }
+
+
+
+
 }
