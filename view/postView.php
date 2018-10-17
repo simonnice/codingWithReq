4b98fb1bc9b389428581129d0b0f76572a0f96ce
sb22ix <sb22ix@student.lnu.se>
2018-10-17 17:08:14 +0200

    Creating a post form in the postView

--- view/postView.php
+++ view/postView.php
@@ -3,4 +3,22 @@ namespace view;
 
 class postView {
 
+    private function generatePostFormHtml() {
+        return '
+			<form method="post" form action="posts">
+				<fieldset>
+					<legend>Write a new post here!</legend>
+					<p id="' . self::$messageId . '">' . $message . '</p>
+
+					<label for="' . self::$title . '">Title :</label>
+					<input type="text" id="' . self::$title . '" name="' . self::$title . '" value="" />
+
+					<label for="' . self::$body . '">Body :</label>
+          <textarea id="' . self::$body . '" name="' . self::$body . '" />
+
+					<input type="submit" name="' . self::$submitPost . '" value="Create Post" />
+				</fieldset>
+			</form>
+		';
+    }
 }
