ea22b9c949494b82edb8ee700ca090117ad79a19
sb22ix <sb22ix@student.lnu.se>
2018-10-17 16:28:08 +0200

    Starting work on Post class following same principles as the User class and register class, checking for errors during creation

--- model/Post.php
+++ model/Post.php
@@ -3,8 +3,22 @@
 class Post {
 
     private $db;
+    private $user_id;
+    private $title;
+    private $body;
+    private $createdAt;
 
-    public function __construct($db) {
+    public function __construct($user_id, $title, $body, $createdAt, $db) {
         $this->db = new Database;
+
+        if (!$title) {
+            throw new \Exception("Title is missing");
+        }
+
+        if (!$body) {
+            throw new \Exception("Body is missing");
+        }
+
     }
+
 }
