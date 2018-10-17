a935f0ac204272bf6893ec850e084b0a9b4d2ffb
sb22ix <sb22ix@student.lnu.se>
2018-10-17 16:34:10 +0200

    Starting work on PostController

--- model/Post.php
+++ model/Post.php
@@ -19,6 +19,19 @@ class Post {
             throw new \Exception("Body is missing");
         }
 
+        $this->user_id = $user_id;
+        $this->title = $title;
+        $this->body = $body;
+        $this->createdAt = $createdAt;
+
+    }
+
+    public function getTitle(): string {
+        return $this->title;
+    }
+
+    public function getBody(): string {
+        return $this->body;
     }
 
 }
