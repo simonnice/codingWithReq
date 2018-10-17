fb92d15be5d6d7dcb98aa608f9e794eb949a31e7
sb22ix <sb22ix@student.lnu.se>
2018-10-17 16:35:51 +0200

    Adding some dependencies to PostController, will check which ones I actually need soon

--- controller/PostController.php
+++ controller/PostController.php
@@ -2,4 +2,17 @@
 namespace controller;
 
 class PostController {
+
+    private $loginView;
+    private $db;
+    private $session;
+    private $cookie;
+
+    public function __construct($login, $db, $session, $cookie) {
+        $this->loginView = $login;
+        $this->db = $db;
+        $this->session = $session;
+        $this->cookie = $cookie;
+    }
+
 }
