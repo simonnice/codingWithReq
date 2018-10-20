f40b037281838579673aec6fe9e28f8323b53f9d
sb22ix <sb22ix@student.lnu.se>
2018-10-20 17:15:07 +0200

    Fixing a small bug with a typo

--- view/LoginView.php
+++ view/LoginView.php
@@ -29,7 +29,7 @@ class LoginView {
     public function generateLoginLink(): string {
         $loginLink = '?';
         return '
-        <a href="' . $loginLink . '">Back to Login</a>
+        <a href="' . $loginLink . '">Back to login</a>
 		';
     }
 
